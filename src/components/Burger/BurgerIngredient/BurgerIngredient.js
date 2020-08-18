b1700a4876ddb001aa8fc3010e5e1f405b15a58f
HeideQueen <lucyfabz@gmail.com>
2020-08-18 01:51:31 -0500

    adding prop type validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.css';
 
@@ -38,6 +39,10 @@ const BurgerIngredient = ({ type }) => {
       <h1>konlulu~</h1>
     </div>
   );
+};
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default BurgerIngredient;
