2b2a54211671d3eb1ea93729a6d65a91adea60b5
HeideQueen <lucyfabz@gmail.com>
2020-09-08 00:00:06 -0500

    fix props

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -28,7 +28,10 @@ const withErrorHandler = (WrappedComponent, axios) => {
     render() {
       return (
         <Fragment>
-          <Modal show={this.state.error} clicked={this.errorConfirmedHandler}>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
             {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />
