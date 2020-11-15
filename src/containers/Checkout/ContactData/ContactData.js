8a1e4ac736a5f2a3b7c67f9965de162eb6a70397
HeideQueen <lucyfabz@gmail.com>
2020-11-14 19:11:05 -0600

    add custom form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -20,6 +20,7 @@ class ContactData extends Component {
         validation: {
           required: true,
         },
+        valid: false,
       },
       street: {
         elementType: 'input',
@@ -31,6 +32,7 @@ class ContactData extends Component {
         validation: {
           required: true,
         },
+        valid: false,
       },
       zipCode: {
         elementType: 'input',
@@ -41,7 +43,10 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true,
+          minLength: 5,
+          maxLength: 5,
         },
+        valid: false,
       },
       country: {
         elementType: 'input',
@@ -53,6 +58,7 @@ class ContactData extends Component {
         validation: {
           required: true,
         },
+        valid: false,
       },
       email: {
         elementType: 'input',
@@ -64,6 +70,7 @@ class ContactData extends Component {
         validation: {
           required: true,
         },
+        valid: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -74,9 +81,6 @@ class ContactData extends Component {
           ],
         },
         value: '',
-        validation: {
-          required: true,
-        },
       },
     },
     loading: false,
@@ -110,6 +114,24 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = false;
+
+    if (rules.required) {
+      isValid = value.trim() !== '';
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.minLength;
+    }
+
+    return isValid;
+  }
+
   inputChangedHandler = (e, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm,
@@ -120,8 +142,12 @@ class ContactData extends Component {
     };
 
     updatedFormElement.value = e.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-
+    console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
   };
 
